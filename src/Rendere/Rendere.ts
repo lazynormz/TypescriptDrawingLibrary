8fabc83a8c6927fcdfbbac53c7e7b5d1adc252c9
Lazynorm <skupi1234@gmail.com>
2022-02-08 12:58:11 +0100

    Created Color.enum and made it so that the user can change color on command

--- src/Rendere/Rendere.ts
+++ src/Rendere/Rendere.ts
@@ -1,25 +1,36 @@
+import { Color } from "./enums/Color"
+
 export class TRendere {
     canvas: HTMLCanvasElement
     ctx: CanvasRenderingContext2D
+    currentColor: string
     constructor(canvas: HTMLCanvasElement) {
         this.canvas = canvas
         this.ctx = <CanvasRenderingContext2D>this.canvas.getContext("2d")
+        this.currentColor = Color.GREY
     }
 
-    fillRect(x: number, y: number, w: number, h: number): void {
-
+    setColor(c?: Color | string): void {
+        if (c) {
+            this.currentColor = c
+            this.ctx.fillStyle = this.currentColor
+        }
     }
 
-    drawRect(x: number, y: number, w: number, h: number): void {
-
+    fillRect(x: number, y: number, w: number, h: number, c?: Color | string): void {
+        this.setColor(c)
     }
 
-    fillCircle(x: number, y: number, w: number, h: number): void {
-
+    drawRect(x: number, y: number, w: number, h: number, c?: Color | string): void {
+        this.setColor(c)
     }
 
-    drawCircle(x: number, y: number, w: number, h: number): void {
+    fillCircle(x: number, y: number, w: number, h: number, c?: Color | string): void {
+        this.setColor(c)
+    }
 
+    drawCircle(x: number, y: number, w: number, h: number, c?: Color | string): void {
+        this.setColor(c)
     }
 
     drawImage(x: number, y: number, w: number, h: number): void {
---
+++ src/Rendere/enums/Color.ts
@@ -0,0 +1,8 @@
+export enum Color {
+    BLACK = "#000000",
+    WHITE = "#ffffff",
+    GREY = "#707070",
+    BLUE = "#0057e3",
+    RED = "#db0b0b",
+    GREEN = "#22cf19"
+}

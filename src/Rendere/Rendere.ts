1f2f3d7d4eeb99bc2c4c999a30c6e314c8e54cfa
Lazynorm <skupi1234@gmail.com>
2022-02-08 13:04:34 +0100

    Base functions have been set, and should work - Test later

--- src/Rendere/Rendere.ts
+++ src/Rendere/Rendere.ts
@@ -19,21 +19,38 @@ export class TRendere {
 
     fillRect(x: number, y: number, w: number, h: number, c?: Color | string): void {
         this.setColor(c)
+        this.ctx.fillRect(x, y, w, h)
     }
 
     drawRect(x: number, y: number, w: number, h: number, c?: Color | string): void {
         this.setColor(c)
+        this.ctx.rect(x, y, w, h)
     }
 
-    fillCircle(x: number, y: number, w: number, h: number, c?: Color | string): void {
+    fillCircle(x: number, y: number, r: number, c?: Color | string): void {
         this.setColor(c)
+        this.ctx.beginPath()
+        this.ctx.arc(x, y, r, 0, 2 * Math.PI)
+        this.ctx.fill()
+        this.ctx.stroke()
     }
 
-    drawCircle(x: number, y: number, w: number, h: number, c?: Color | string): void {
+    drawCircle(x: number, y: number, r: number, c?: Color | string): void {
         this.setColor(c)
+        this.ctx.beginPath()
+        this.ctx.arc(x, y, r, 0, 2 * Math.PI)
+        this.ctx.stroke()
     }
 
-    drawImage(x: number, y: number, w: number, h: number): void {
+    drawImage(image: string, x: number, y: number, w?: number, h?: number): void {
+        let img = new Image()
+        img.src = image
 
+        img.onload = () => {
+            if(h && w){
+                return this.ctx.drawImage(img, x, y, w, h)
+            }
+            this.ctx.drawImage(img, x, y)
+        }
     }
 }
